1bc46d813623da58048020d15dd6633b5e827a26
Daniel Getial <danielg.soft29@gmail.com>
2026-07-07 00:02:19 -0500

    modificacion: en consultas_busqueda.sql, consultas_DML.sql y PoblarTablas.sql

--- DML/consultas_busqueda.sql
+++ DML/consultas_busqueda.sql
@@ -12,12 +12,34 @@ INNER JOIN tbZona Z ON (M.id_zona = Z.id_Zona)
 order by id_Zona
 
 -- Consulta para ver TODOS los registros de tbMenu
-SELECT id_Categoria, 
+SELECT id_Menu, 
 	   id_Categoria,
 	   Producto, 
 	   Precio, 
 	   DescripcionP
 FROM tbMenu
+
+--- Consulta las diferentes categorias que tenemos en tbmenu
+SELECT DISTINCT id_Categoria
+FROM tbMenu
+
+-- Consulta para ver las categorias que aun faltan en tbMenu
+SELECT DISTINCT C.id_Categoria AS Categorias_tbCategorias, 
+				M.id_Categoria AS Categorias_tbMenu,
+				C.NombreCategoria
+FROM tbCategorias C 
+LEFT JOIN tbMenu M
+	ON (M.id_Categoria = C.id_Categoria)
+--WHERE M.id_Categoria IS NULL
+
+--- Consultar tbMenu pero solo las que son de una categoria especifica
+SELECT id_Menu, 
+	   id_Categoria,
+	   Producto, 
+	   Precio, 
+	   DescripcionP
+FROM tbMenu
+WHERE id_Categoria = 4
 
 -- Para ver TODOS los registros de tbCategorias
 SELECT id_Categoria,
