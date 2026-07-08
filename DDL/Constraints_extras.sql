d77959e0f2f9a37fc3e2270a297863f73e067d6e
Daniel Getial <danielg.soft29@gmail.com>
2026-07-08 13:18:26 -0500

    Agrego nuevo CHECK en tbDetallePedido para el campo PrecioUnitario

--- DDL/Constraints_extras.sql
+++ DDL/Constraints_extras.sql
@@ -4,6 +4,8 @@
 		del mismo numero de mesa con el mismo numero de zona, como si la mesa existiria dos veces en la misma zona
 
 	-- Cambio de campo id_Cliente NOT NULL por NULL en tbPedidos
+
+	** Agrego un CHECK a tbDetallePedido al campo PrecioUnirario para asegurar su valor positivo
 */
 
 USE BdRamadaValluna
@@ -15,6 +17,11 @@ GO
 ALTER TABLE tbMesa
 	ADD CONSTRAINT Uq_tbMesa_idZona_NumMesa 
 	UNIQUE (id_Zona, NumMesa);
+
+-- Unique para tbDetallePedido
+ALTER TABLE tbDetallePedido
+	ADD CONSTRAINT CK_tbDetallePedido_PrecioUnitario
+	CHECK (PrecioUnitario > 0)
 
 -- Cambiar campo NOT NULL por NULL
 AlTER TABLE tbPedidos
